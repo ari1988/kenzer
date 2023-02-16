de71a6cc3227de6d92970de2a6fcdb8c5feb6d0f
glatisant <26509147+g147@users.noreply.github.com>
2023-02-16 19:55:46 +0530

    kenzer[3.68]

--- chrome.sh
+++ chrome.sh
@@ -1,6 +1,11 @@
-wget  http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_109.0.5414.119-1_amd64.deb 
-sudo dpkg -i google-chrome-stable_109.0.5414.119-1_amd64.deb
-wget https://chromedriver.storage.googleapis.com/109.0.5414.74/chromedriver_linux64.zip
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+wget https://chromedriver.storage.googleapis.com/110.0.5481.30/chromedriver_linux64.zip
+
+#wget  http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_109.0.5414.119-1_amd64.deb 
+#sudo dpkg -i google-chrome-stable_109.0.5414.119-1_amd64.deb
+#wget https://chromedriver.storage.googleapis.com/109.0.5414.74/chromedriver_linux64.zip
+
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
 chmod +x /usr/bin/chromedriver
